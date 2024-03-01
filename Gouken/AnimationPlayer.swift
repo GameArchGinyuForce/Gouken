aebca2878799447cc207848a6494d986880a2384
imnorookie <liquid.snake2k20@gmail.com>
2024-02-29 23:16:21 -0800

    GK-38 Add stop animation functionality.

--- Gouken/AnimationPlayer.swift
+++ Gouken/AnimationPlayer.swift
@@ -10,7 +10,11 @@ import SceneKit
 
 // Abruptly stops an animation on a SCNNode to play another.
 func playAnimation(onNode node: SCNNode, withSCNFile anim: String, andBlendOutDuration blendOut: CGFloat = CGFloat(0.0)) {
-    node.removeAllAnimations(withBlendOutDuration: blendOut)
+    StopAnimation(onNode: node, withBlendOutDuration: blendOut)
     let animPlayer = SCNAnimationPlayer.loadAnimation(fromSceneNamed: anim)
     node.addAnimationPlayer(animPlayer, forKey: anim)
+}
+
+func StopAnimation(onNode node: SCNNode, withBlendOutDuration blendOut: CGFloat = CGFloat(0.0)) {
+    node.removeAllAnimations(withBlendOutDuration: blendOut)
 }
