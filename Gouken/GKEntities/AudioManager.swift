7555d5af08c20948d42e74192b9c140a66b3b55d
Nathan Dong <ndong5@my.bcit.ca>
2024-03-28 15:38:36 -0700

    base AudioManager functionality

--- Gouken/GKEntities/AudioManager.swift
+++ Gouken/GKEntities/AudioManager.swift
@@ -7,9 +7,15 @@
 
 import AVFoundation
 
-// Singleton AudioManager
-// Call methods via AudioManager.Instance().methodName()
-// Can be accessed globally
+
+/*
+ // Singleton AudioManager
+ // Call methods via AudioManager.Instance().methodName()
+ // Can be accessed globally
+ 
+ TODO: Consider loading all possible AVAudioPlayer sounds when loading the game scene
+ TODO: Consider multiple channels for each player
+ */
 class AudioManager {
     
     // Initial Implementation, consider better design
@@ -61,6 +67,82 @@ class AudioManager {
         player1AudioChannelPlayer?.stop();
         player2AudioChannelPlayer?.stop();
         effectsAudioChannelPlayer?.stop();
+    }
+    
+    func playBackgoundMusicSoundByURL(fileName: String, ext: String = ".mp3") {
+        
+        backgroundMusicChannelPlayer?.stop();
+        
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: ext) else {
+            print("Could not find " + fileName + ext)
+            return
+        }
+        
+        do {
+            backgroundMusicChannelPlayer = try AVAudioPlayer(contentsOf: url)
+            backgroundMusicChannelPlayer?.numberOfLoops = -1 // Loop indefinitely
+            backgroundMusicChannelPlayer?.volume = 0.3
+            backgroundMusicChannelPlayer?.play()
+        } catch let error {
+            print("Error creating audio player: \(error.localizedDescription)")
+        }
+    }
+    
+    func playPlayer1SoundByURL(fileName: String, ext: String = ".mp3") {
+        
+        player1AudioChannelPlayer?.stop();
+        
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: ext) else {
+            print("Could not find " + fileName + ext)
+            return
+        }
+        
+        do {
+            player1AudioChannelPlayer = try AVAudioPlayer(contentsOf: url)
+            player1AudioChannelPlayer?.numberOfLoops = 1 // Play once
+            player1AudioChannelPlayer?.volume = 0.3
+            player1AudioChannelPlayer?.play()
+        } catch let error {
+            print("Error creating audio player: \(error.localizedDescription)")
+        }
+    }
+    
+    func playPlayer2SoundByURL(fileName: String, ext: String = ".mp3") {
+        
+        player2AudioChannelPlayer?.stop();
+        
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: ext) else {
+            print("Could not find " + fileName + ext)
+            return
+        }
+        
+        do {
+            player2AudioChannelPlayer = try AVAudioPlayer(contentsOf: url)
+            player2AudioChannelPlayer?.numberOfLoops = 1 // Play once
+            player2AudioChannelPlayer?.volume = 0.3
+            player2AudioChannelPlayer?.play()
+        } catch let error {
+            print("Error creating audio player: \(error.localizedDescription)")
+        }
+    }
+    
+    func playEffectSoundByURL(fileName: String, ext: String = ".mp3") {
+        
+        effectsAudioChannelPlayer?.stop();
+        
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: ext) else {
+            print("Could not find " + fileName + ext)
+            return
+        }
+        
+        do {
+            effectsAudioChannelPlayer = try AVAudioPlayer(contentsOf: url)
+            effectsAudioChannelPlayer?.numberOfLoops = 1 // Play once
+            effectsAudioChannelPlayer?.volume = 0.3
+            effectsAudioChannelPlayer?.play()
+        } catch let error {
+            print("Error creating audio player: \(error.localizedDescription)")
+        }
     }
 
 }
