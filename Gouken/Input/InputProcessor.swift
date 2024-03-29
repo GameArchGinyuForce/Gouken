f64cb88a54932eb5c090434de6b33af5d2e8befe
imnorookie <liquid.snake2k20@gmail.com>
2024-03-28 18:54:10 -0700

    GK-48 Thread-Safe Ring Buffer.

--- Gouken/Input/InputProcessor.swift
+++ Gouken/Input/InputProcessor.swift
@@ -2,7 +2,35 @@
 //  InputProcessor.swift
 //  Gouken
 //
-//  Created by Jaskaran Chahal on 2024-03-28.
+//  Created by Jas Singh on 2024-03-28.
 //
 
-import Foundation
+
+let bufferSize = 60
+let P1Buffer = InputBuffer()
+let P2Buffer = InputBuffer()
+//let moveSequences : [[ButtonType]] = [[ButtonType.Down, ButtonType.Right, ButtonType.LP]]
+
+/** A thread safe input buffer. */
+actor InputBuffer {
+    var lastInput : ButtonType = ButtonType.Neutral
+    var input : [ButtonType] = Array(repeating: ButtonType.Neutral, count: bufferSize)
+    var writeIdx : Int = 0
+    
+    func insertInput(withPress: ButtonType) {
+        lastInput = withPress
+    }
+    
+    func updateInput() {
+        input[writeIdx] = lastInput
+        writeIdx = (writeIdx + 1) % bufferSize
+        lastInput = ButtonType.Neutral
+    }
+    
+    func processInput() {
+        updateInput()
+        
+    }
+}
+
+
