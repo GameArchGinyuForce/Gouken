380e41ccc6e2ceb6b8f7136d027f5f78c39d2960
imnorookie <liquid.snake2k20@gmail.com>
2024-04-02 20:25:07 -0700

    Add working Shoryuken input and attack. Still requires hitbox/hurtbox adjustments.

--- Gouken/AnimationList.swift
+++ Gouken/AnimationList.swift
@@ -15,6 +15,7 @@ let characterAnimations : Dictionary = [
         CharacterState.Downed       :   "art.scnassets/Synty_Frank_RPG_Katana_Hit_Knockdown_Loop.dae",
         CharacterState.Stunned      :   "art.scnassets/Synty_Frank_RPG_Katana_Hit01.dae",
         CharacterState.DashingLeft      :   "art.scnassets/Synty_Frank_RPG_Katana_Step_B__IP.dae",
-        CharacterState.DashingRight      :   "art.scnassets/Synty_Frank_RPG_Katana_Step_F__IP.dae"
+        CharacterState.DashingRight      :   "art.scnassets/Synty_Frank_RPG_Katana_Step_F__IP.dae",
+        CharacterState.DragonPunch  : "art.scnassets/Synty_Frank_RPG_Katana_S1_Skill02__IP.dae"
     ]
 ]
